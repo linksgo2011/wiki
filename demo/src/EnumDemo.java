8160679f16f5ef444ab45b8aded75fea5cd61e99
Lin Ning <nlin@thoughtworks.com>
2019-10-17 10:40:07 +0800

    Add k8s and else

--- demo/src/EnumDemo.java
+++ demo/src/EnumDemo.java
@@ -1,12 +1,11 @@
 package src;
-
 /**
  * Created by nlin on 7/7/17.
  */
 public class EnumDemo {
     public static void main(String args[]) {
         try{
-            Day day = Day.valueOf("");
+            String day = String.valueOf("");
             System.out.print(day);
         }catch (Exception e){
             e.printStackTrace();
